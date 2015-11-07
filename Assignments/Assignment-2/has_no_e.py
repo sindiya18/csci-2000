d76749db3f38b9dfdef8e8cf08d5823ba654fdb3
sindiya18 <sindiya18@github.com>
2015-11-07 12:42:28 -0500

    Assigment 2 : completed has_no_e.py function

--- Assignments/Assignment-2/has_no_e.py
+++ Assignments/Assignment-2/has_no_e.py
@@ -1,15 +1,15 @@
 
 def has_no_e(string): #defining a has_no_e function
-    count_e = 0 #defining variable to count letter e
+    ecount = 0 #defining variable to count letter e
 
     for char in txtfile:
         if char == 'e': #if char e is in file count increases
-            count_e += 1
+            ecount += 1
             string = reader.read()
-    if count_e == 0: #if count is  zero, letter e not found
-        return 'True Gadsby_Stripped.txt Does Not Contains The Letter e'
+    if ecount == 0: #if count is  zero, letter e not found
+        return 'True Gadsby_Stripped.txt does not have letter e'
     else:  #else  char e is in the file
-        return 'False  Gadsby_Stripped.txt Contains The Letter e'  
+        return 'False  Gadsby_Stripped.txt has the letter e'  
 
 reader = open('gadsby_stripped.txt', 'r')
 txtfile = reader.read()
